388d8b108b11e30a724761acf8b683efa32ca709
iroj123 <irojacharya6@gmail.com>
2025-10-09 20:28:47 +0545

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","")for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+setup(
+name='mlproject',
+version='0.0.1',
+author="Iroj",
+author_email="irojacharya6@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+include_package_data=True,  
+package_data={'': ['requirements.txt']} 
+)
+
+
+
+
+
